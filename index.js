0515e9d30f83b1f57d4b23638cf1f129ec9be106
Ankit Raghuvanshi <ankit.raghuvanshi@nl.abnamro.com>
2018-06-06 17:11:57 +0200

    change

--- index.js
+++ index.js
@@ -1,117 +1,18 @@
-// dependencies
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('https');
-var unirest = require("unirest");
-let errorResposne = {
-    results: []
-};
-var port = process.env.PORT || 8080;
-// create serve and configure it.
-const server = express();
-server.use(bodyParser.json());
-server.post('/getMovies',function (request,response)  {
-    if(request.body.result.parameters['top-rated']) {
-        var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-            req.query({
-                "page": "1",
-                "language": "en-US",
-                "api_key": ""
-            });
-            req.send("{}");
-            req.end(function(res) {
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else if(res.body.results.length > 0) {
-                    let result = res.body.results;
-                    let output = '';
-                    for(let i = 0; i<result.length;i++) {
-                        output += result[i].title;
-                        output+="\n"
-                    }
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    })); 
-                }
-            });
-    } else if(request.body.result.parameters['movie-name']) {
-     //   console.log('popular-movies param found');
-        let movie = request.body.result.parameters['movie-name'];
-        var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
-            req.query({
-                "include_adult": "false",
-                "page": "1",
-                "query":movie,
-                "language": "en-US",
-                "api_key": ""
-            });
-            req.send("{}");
-            req.end(function(res) {
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else if(res.body.results.length > 0) {
-                let result = res.body.results[0];
-                let output = "Average Rating : " + result.vote_average + 
-                "\n Plot : " + result.overview + "url" + result.poster_path
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    }));
-                } else {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Couldn't find any deatails. :(  ",
-                        "displayText" : "Couldn't find any deatails. :(  "
-                    }));
-                }
-            });
+const app = new DialogflowApp({request: request, response: response});
+const WELCOME_INTENT = 'input.welcome';
+const NUMBER_INTENT = 'input.number';
 
-    } else if(request.body.result.parameters['popular-movies']) {    
-        var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
-            req.query({
-                "page": "1",
-                "language": "en-US",
-                "api_key": ""
-            });
-            req.send("{}");
-            req.end(function(res){
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else {
-                    let result = res.body.results;
-                    let output = '';
-                    for(let i = 0; i < result.length;i++) {
-                        output += result[i].title;
-                        output+="\n"
-                    }
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    })); 
-                }
-            });
-    }
-});
-server.get('/getName',function (req,res){
-    res.send('Swarup Bam');
-});
-server.listen(port, function () {
-    console.log("Server is up and running...");
-});
+function welcomeIntent (app) {
+    app.ask('Welcome to action snippets! Say a number.',
+        ['Say any number', 'Pick a number', 'We can stop here. See you soon.']);
+}
+
+function numberIntent (app) {
+    const number = app.getArgument(NUMBER_ARGUMENT);
+    app.tell('You said ' + number);
+}
+
+const actionMap = new Map();
+actionMap.set(WELCOME_INTENT, welcomeIntent);
+actionMap.set(NUMBER_INTENT, numberIntent);
+app.handleRequest(actionMap);
